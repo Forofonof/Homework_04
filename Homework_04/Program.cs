16e3a244e9c2ffd1da849f62f2258b9b9616600f
Forofonof <DanielResult1@outlook.com>
2022-04-01 19:10:46 +0300

    Добавьте файлы проекта.

---
+++ Homework_04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Вячеслав";
+        string surname = "Форофонов";
+        string temporary;
+        Console.WriteLine($"Ваше Имя и Фамилия: {surname} {name}");
+        temporary = name;
+        name = surname;
+        surname = temporary;
+        Console.WriteLine($"Ваше Имя и Фамилия: {surname} {name}");
+    }
+}
